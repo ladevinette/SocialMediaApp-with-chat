15ec286215a0ef52d3955322d037f4528c0539fe
Bartek <bartekszalowski59@gmail.com>
2023-03-12 15:30:54 +0100

    deploy

--- hooks/useSocket.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState, useRef } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { io, Socket } from "socket.io-client";
-
-export const useSocket = () => {
-  const socketClient = useRef<SocketIOClient.Socket>();
-
-  return <div></div>;
-};
